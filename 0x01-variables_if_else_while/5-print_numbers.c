4623f5e53d1c54c12af2d3a04bae3afbd8f74be1
Sally-Fullstack <mambonganz@outlook.com>
2023-07-15 08:36:20 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-Positive anything is better than negative nothing
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}
